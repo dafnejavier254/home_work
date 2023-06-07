2b96fbc38f5179e1fb9e59e425b3f8a518fd354c
Dafne Javier <dafnejavier987@gmail.com>
2023-06-07 01:39:21 +0000

    nano

--- python_script.py
+++ python_script.py
@@ -1,1 +1,11 @@
+def main():
 
+   x = ["this is a sentence", "this is another", "this is a third sentence"]
+
+
+for thing in x:
+
+
+print('this is a string:' + str(x))
+if_name_=='_main_':
+main()
